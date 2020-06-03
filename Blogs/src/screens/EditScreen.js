13d48a5a00c8516974270344e8666440fda33a0f
whitedevil31 <63295747+whitedevil31@users.noreply.github.com>
2020-06-03 19:54:19 +0530

    Update EditScreen.js

--- Blogs/src/screens/EditScreen.js
+++ Blogs/src/screens/EditScreen.js
@@ -1,19 +1,17 @@
 import React, { useState, useContext } from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import BlogPostForm from "../components/BlogPostForm";
-import Context from "../context/BlogContext";
+import { Context } from "../context/BlogContext";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 const EditScreen = ({ navigation, route }) => {
   const { id, title, content } = route.params;
-  const { editBlogPost } = useContext(Context);
+  const items = { title, content };
 
   return (
     <BlogPostForm
-      initialvalue={(title, content)}
-      onSubmit={(title, content) => {
-        editBlogPost(id, title, content, () => navigation.pop());
-      }}
+      intialValue={{ title: items.title, content: items.content }}
+      onSubmit={(title, content) => console.log(title, content)}
     />
   );
 };
